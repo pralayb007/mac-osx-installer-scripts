b89f4f8f67aa1483d789be98c9d5e6c6a849a064
Pralay Biswas <pralayb@uci.edu>
2020-11-14 21:33:32 -0800

    Adding the script to force XCode installation

--- master.sh
+++ master.sh
@@ -2,3 +2,9 @@
 source ./utilities/echo_utils.sh
 
 echo_green "Install starting. You may be asked for your password (for sudo)."
+
+#######################################
+# Require Xcode installation          #
+#######################################
+echo_yellow "Beginning XCode installation"
+sh ./setup/xcode-install.sh
---
+++ setup/xcode-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+source ./utilities/echo_utils.sh
+
+#######################################
+# Require Xcode installation          #
+#######################################
+echo_yellow "In order to install Xcode, log into App Store using your appleId"
+echo_yellow "Press any key once done"
+read -r varname
+
+xcode-select --install
+
+if xcode-select -p | grep -q 'CommandLineTools\|Xcode.app'; then
+  echo_green "XCode was installed"
+else
+  echo_red "XCode was not installed"
+fi
