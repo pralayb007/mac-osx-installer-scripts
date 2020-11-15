087a0d067a0834735c5bfab6e3812741b768bf4e
Pralay Biswas <pralayb@uci.edu>
2020-11-15 03:17:18 -0800

    Add perl, awk, sed and grep utils

--- installations/gnu-utils-install.sh
+++ installations/gnu-utils-install.sh
@@ -4,3 +4,7 @@
 brew install autoconf     # Automatically configure software source code packages
 brew install automake     # Tool for generating GNU Standards-compliant Makefiles
 brew install gettext      # GNU internationalization (i18n) and localization (l10n) library
+brew install gnu-sed
+brew install grep
+brew install perl         # Not really GNU but still
+brew install awk          # Not really GNU but still
