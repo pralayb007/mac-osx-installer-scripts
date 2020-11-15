c435c368dddc1fe7e4d0a81241a0baaa3937b1fe
Pralay Biswas <pralayb@uci.edu>
2020-11-14 23:29:48 -0800

    Add homebrew module and update master script to use this new module

--- master.sh
+++ master.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 source ./utilities/echo_utils.sh
+source ./utilities/printf_utils.sh
 
 echo_green "Install starting. You may be asked for your password (for sudo)."
-
+printf_new "#" 50
 #######################################
 # Require Xcode installation          #
 #######################################
-echo_yellow "Beginning XCode installation"
+echo_red "Beginning XCode installation"
 sh ./setup/xcode-install.sh
+echo_green "Done with XCode installation"
+printf_new "#" 50
+
+#######################################
+# Homebrew installation and tapping   #
+#######################################
+echo_red "Beginning Homebrew installation"
+sh ./setup/homebrew-install.sh
+echo_green "Done with Homebrew setup"
+printf_new "#" 50
---
+++ setup/homebrew-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+source ./utilities/echo_utils.sh
+
+#######################################
+# Homebrew installation and tapping   #
+#######################################
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+if hash brew &> /dev/null; then
+	echo_green "Homebrew already installed"
+else
+    echo_yellow "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update && brew cleanup
+
+# Tapping into various brews
+brew tap homebrew/cask-versions
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
