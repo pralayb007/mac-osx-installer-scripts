22c550b2572e19caa5184adf54f04f22e0481ddd
Pralay Biswas <pralayb@uci.edu>
2020-11-14 23:24:48 -0800

    Refactor colors as a util module and have echo_utils use it

---
+++ utilities/color_utils.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+color_off='\033[0m'
+boldRed='033[1;31m'
+boldGreen='\033[1;32m'
+boldYellow='\033[1;33m'
--- utilities/echo_utils.sh
+++ utilities/echo_utils.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+source ./utilities/color_utils.sh
 
 # Echo helper utils
 # show text in green
-function echo_green { echo -e '\033[1;32m'"$1"'\033[0m'; }
+function echo_green { echo "$green""$1""$color_off"; }
 # show text in yellow
-function echo_yellow { echo -e '\033[1;33m'"$1"'\033[0m'; }
+function echo_yellow { echo "$yellow""$1""$color_off"; }
 # show text in red
-function echo_red  { echo -e '\033[1;31m'"$1"'\033[0m'; }
+function echo_red  { echo "$red""$1""$color_off"; }
