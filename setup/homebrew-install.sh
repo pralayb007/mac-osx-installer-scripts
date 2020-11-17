c22cc45a79dc3faee7aafd5db01546d4adc1dbaa
Pralay Biswas <pralayb@uci.edu>
2020-11-17 03:40:43 -0800

    Replace tabs with spaces in homebrew installation module

--- setup/homebrew-install.sh
+++ setup/homebrew-install.sh
@@ -6,15 +6,15 @@ source ../utilities/echo_utils.sh
 #######################################
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 if hash brew &> /dev/null; then
-	echo_green "Homebrew already installed"
+  echo_green "Homebrew already installed"
 else
     echo_yellow "Installing homebrew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update && brew cleanup
 
 # Tapping into various brews
-brew tap homebrew/cask-versions
 brew tap homebrew/cask
+brew tap homebrew/cask-versions
 brew tap homebrew/cask-fonts
