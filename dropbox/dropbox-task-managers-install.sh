946c51fd32221a29d102a52728b4e47020c9baaa
Pralay Biswas <pralayb@uci.edu>
2020-11-17 04:57:05 -0800

    Fix some more unmounting issues with task manager dropbox modules

--- dropbox/dropbox-task-managers-install.sh
+++ dropbox/dropbox-task-managers-install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-source ./utilities/echo_utils.sh
+source ../utilities/echo_utils.sh
 
 # OmniFocus
 echo_yellow "Installing Omnifocus from dropbox"
 hdiutil attach ~/Dropbox/Working_apps/OmniFocus.dmg
 ditto -rsrc /Volumes/OmniFocus*/OmniFocus*.app /Applications/OmniFocus.app
+hdiutil unmount /Volumes/OmniFo*
 echo_red "Manually unmount DMGs on Finder and click any key to continue"
 read varname
