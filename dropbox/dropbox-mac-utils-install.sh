6c6fe11ae14d6cc5d160563710813df59ce44204
Pralay Biswas <pralayb@uci.edu>
2020-11-17 04:54:19 -0800

    Fix the unmounting issues with some mac util DMGs

--- dropbox/dropbox-mac-utils-install.sh
+++ dropbox/dropbox-mac-utils-install.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-source ./utilities/echo_utils.sh
+source ../utilities/echo_utils.sh
 
 # Hook
 hdiutil attach ~/Dropbox/Working_apps/Hook.dmg
 ditto -rsrc /Volumes/Hook*.*/Hook.app /Applications/Hook.app
+hdiutil unmount /Volumes/*productivity*.*
 
 # Hookshot (window resizing manager)
 echo_yellow "Installing Hookshot from dropbox"
 hdiutil attach ~/Dropbox/Working_apps/Hookshot.dmg
 ditto -rsrc /Volumes/Hookshot*.*/Hookshot.app /Applications/Hookshot.app
+hdiutil unmount /Volumes/Hookshot*
 
 # Bartender
 hdiutil attach ~/Dropbox/Working_apps/Bartender.dmg
 ditto -rsrc /Volumes/Bartender*/Bartender*.app /Applications/Bartender.app
+hdiutil unmount /Volumes/Bartender*.*
 
 echo_red "Manually eject all DMGs on Finder and press any key to continue"
 read -r varname
